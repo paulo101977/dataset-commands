ec7379bae9156f78d8419fe57c883281b961d165
paulo10.1977 <paulo10.1977@yahoo.com.br>
2024-06-26 22:59:08 -0300

    Adicionando lista de comandos e notebook para ML

---
+++ commands/commands.py
@@ -0,0 +1,8 @@
+commands_list = [
+    "Sim", "Não", "Cima", "Baixo", "Esquerda",
+    "Direita", "Ligado", "Desligado", "Parar", "Vai",
+    "Zero", "Um", "Dois", "Três", "Quatro", "Cinco", "Seis",
+    "Sete", "Oito", "Nove", "Cama", "Pássaro", "Gato", "Cachorro", 
+    "Feliz", "Casa", "Marvin", "Sheila", "Árvore", "Uau", "Trás", 
+    "Avançar", "Seguir", "Aprender", "Visual"
+]
